70d264ca6a7a97d45db19b6e0e87af5e5e06df89
Spatika <spatika.n@gmail.com>
2013-04-23 17:58:09 +0800

    View just displays title of book

---
+++ TestProject/books/views.py
@@ -0,0 +1,15 @@
+# Create your views here.
+from django.http import HttpResponse
+from books.models import Books
+#from django.template import Context, loader     
+def index(request):
+	books_list = Books.objects.all()
+	output = ','.join([b.title for b in books_list, b.author for b in books_list])
+	return HttpResponse(output)
+    
+
+	
+      
+
+     
+    
